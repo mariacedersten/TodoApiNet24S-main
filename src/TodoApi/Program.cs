c901740ebfc353c526bfda6517273e8fd0b89071
mariacedersten <s2401554@edu.bc.fi>
2025-05-31 21:10:58 +0300

    Vaihdoit paikallisen muistin tietokannan pilvipohjaiseen tietokantaan Azuren palvelussa

--- src/TodoApi/Program.cs
+++ src/TodoApi/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<TodoContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
